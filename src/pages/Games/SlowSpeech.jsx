860e7927f1797442c66a5d5d941f136130c9133a
CamrynW <camryn.wutke@alumni.ubc.ca>
2025-03-08 19:47:43 -0800

    added some games

---
+++ src/pages/Games/SlowSpeech.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SlowSpeech() {
+    return (
+        <div>
+          <h3>Slow Speech</h3>
+        </div>
+    );
+}
+
+export default SlowSpeech;
---
+++ src/pages/Games/TongueTwisters.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TongueTwisters() {
+    return (
+        <div>
+          <h3>Tongue twisters</h3>
+        </div>
+    );
+}
+
+export default TongueTwisters;
