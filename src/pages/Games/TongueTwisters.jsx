fcc7aac6264cfc1b2de09a29861e2a8905687335
CamrynW <camryn.wutke@alumni.ubc.ca>
2025-03-08 20:04:19 -0800

    edited games

--- src/pages/Games/TongueTwisters.jsx
+++ src/pages/Games/TongueTwisters.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 function TongueTwisters() {
+  // want bar to show progress 
+
+  // listening
+
+  // together
+
+  // Sound fades out
+
+  // speak alone
     return (
         <div>
           <h3>Tongue twisters</h3>
