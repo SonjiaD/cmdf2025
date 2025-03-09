fed6b59921ab0b0dd0cbb9860aecf68cf60917cb
CamrynW <camryn.wutke@alumni.ubc.ca>
2025-03-08 18:57:27 -0800

    worked on exercises

--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Fluency from "./Fluency";
 
 function Exercises() {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
   return <>
     <h3>What would you like to work on today?</h3>
     <div className="board-row">
---
+++ src/pages/Fluency.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Fluency() {
+    return (
+        <div>
+          <h1>Fluency practice</h1>
+        </div>
+    );
+}
+
+export default Fluency;
