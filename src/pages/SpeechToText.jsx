d8783753c858577344d7ac0229affc63282959b1
CamrynW <camryn.wutke@alumni.ubc.ca>
2025-03-09 08:24:32 -0700

    accuracy now changes

--- src/pages/SpeechToText.jsx
+++ src/pages/SpeechToText.jsx
@@ -12,6 +12,36 @@ const SpeechToText = () => {
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const { progress, increaseProgress, setMatchAccuracy } = useProgressStore();
 
+  // Levenshtein Distance function to compare strings (text comparison)
+ const getLevenshteinDistance = (a, b) => {
+  const tmp = [];
+  for (let i = 0; i <= a.length; i++) {
+    tmp[i] = [i];
+  }
+  for (let j = 0; j <= b.length; j++) {
+    tmp[0][j] = j;
+  }
+  for (let i = 1; i <= a.length; i++) {
+    for (let j = 1; j <= b.length; j++) {
+      tmp[i][j] = Math.min(
+        tmp[i - 1][j] + 1,
+        tmp[i][j - 1] + 1,
+        tmp[i - 1][j - 1] + (a[i - 1] === b[j - 1] ? 0 : 1)
+      );
+    }
+  }
+  return tmp[a.length][b.length];
+};
+
+// List of longer tongue-twisters to practice
+const words = [
+    "Peter Piper picked a peck of pickled peppers",
+    "She sells seashells by the seashore",
+    "How much wood would a woodchuck chuck if a woodchuck could chuck wood?",
+    "Fuzzy Wuzzy was a bear, Fuzzy Wuzzy had no hair",
+    "Betty Botter bought some butter, but she said the butter’s bitter"
+  ];
+  
   useEffect(() => {
     if ("webkitSpeechRecognition" in window) {
       const speechRecognition = new window.webkitSpeechRecognition();
@@ -87,8 +117,6 @@ const SpeechToText = () => {
     <h3 className="text-xl">Target Phrase: <span className="font-semibold">{targetWord}</span></h3>
   </div>
 </div>
-
-
 
       <button
         onClick={isRecording ? stopRecording : startRecording}
