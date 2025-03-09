069e7b8248ff5f9d9dffc05a0e705a6d5ccd19a4
CamrynW <camryn.wutke@alumni.ubc.ca>
2025-03-08 18:35:10 -0800

    working on exercises

--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -3,7 +3,14 @@ import React from "react";
 function Exercises() {
   return <>
     <h3>What would you like to work on today?</h3>
-    <button className="square">X</button>
+    <div className="board-row">
+        <button className="square">Articulation</button>
+        <button className="square">Fluency</button>
+      </div>
+      <div className="board-row">
+        <button className="square">Speaking</button>
+        <button className="square">Understanding</button>
+      </div>
     </>;
 }
 
