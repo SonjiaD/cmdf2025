982f5121a0f37fffa4d19058e5d002dc1f8f6072
Jamille <Vicentee.jamille@gmail.com>
2025-03-09 01:25:04 -0800

    fixed nav bar + exercise gap

--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import './Exercises.css';
 
 function Exercises() {
   const navigate = useNavigate();
 
   return (
     <>
-      <h3>What would you like to work on today?</h3>
+      <h3 className="exercises-Title">What would you like to work on today?</h3>
       <div className="board-row">
         <button
           className="board-box board-box-articulation"
